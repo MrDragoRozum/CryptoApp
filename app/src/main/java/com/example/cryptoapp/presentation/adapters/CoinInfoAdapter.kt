ed0ef6f5f5be989291bf4371c41586ecec94247e
Alexander Rozum <roozumaleks@gmail.com>
2023-09-22 14:19:50 +0300

    Реализованы ListAdapter, CoinItemDiffCallback и лямбда вместо интерфейса

--- app/src/main/java/com/example/cryptoapp/presentation/adapters/CoinInfoAdapter.kt
+++ app/src/main/java/com/example/cryptoapp/presentation/adapters/CoinInfoAdapter.kt
@@ -2,27 +2,17 @@ package com.example.cryptoapp.presentation.adapters
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import androidx.recyclerview.widget.ListAdapter
 import com.bumptech.glide.Glide
 import com.example.cryptoapp.R
 import com.example.cryptoapp.databinding.ItemCoinInfoBinding
 import com.example.cryptoapp.domain.entities.CoinPriceInfo
 
-class CoinInfoAdapter : RecyclerView.Adapter<CoinInfoAdapter.CoinInfoViewHolder>() {
+class CoinInfoAdapter : ListAdapter<CoinPriceInfo, CoinInfoViewHolder>(CoinItemDiffCallback()) {
 
-    var coinInfoList: List<CoinPriceInfo> = listOf()
-        set(value) {
-            field = value
-            notifyDataSetChanged()
-        }
-
-    var onCoinClickListener: OnCoinClickListener? = null
+    var onCoinClickListener: ((coin: CoinPriceInfo) -> (Unit))? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CoinInfoViewHolder {
-//        val view = LayoutInflater
-//            .from(parent.context)
-//            .inflate(R.layout.item_coin_info, parent, false)
         val view = ItemCoinInfoBinding.inflate(
             LayoutInflater.from(parent.context),
             parent,
@@ -31,10 +21,8 @@ class CoinInfoAdapter : RecyclerView.Adapter<CoinInfoAdapter.CoinInfoViewHolder>
         return CoinInfoViewHolder(view)
     }
 
-    override fun getItemCount() = coinInfoList.size
-
     override fun onBindViewHolder(holder: CoinInfoViewHolder, position: Int) {
-        val coin = coinInfoList[position]
+        val coin = getItem(position)
         with(holder) {
             with(binding) {
                 with(coin) {
@@ -42,24 +30,19 @@ class CoinInfoAdapter : RecyclerView.Adapter<CoinInfoAdapter.CoinInfoViewHolder>
                         itemView.context.resources.getString(R.string.symbols_template)
                     val lastUpdatedTemplate =
                         itemView.context.resources.getString(R.string.last_updated_template)
-                    textViewSymbols.text =
-                        String.format(symbolsTemplate, fromSymbol, toSymbol)
+
+                    textViewSymbols.text = String.format(symbolsTemplate, fromSymbol, toSymbol)
                     textViewPrice.text = price
                     textViewUpdated.text = String.format(lastUpdatedTemplate, lastUpdate)
                     Glide.with(itemView.context)
                         .load(imageUrl)
                         .into(imageViewLogoCoin)
+
                     itemView.setOnClickListener {
-                        onCoinClickListener?.onCoinClick(this)
+                        this@CoinInfoAdapter.onCoinClickListener?.invoke(this)
                     }
                 }
             }
         }
-    }
-
-    inner class CoinInfoViewHolder(val binding: ItemCoinInfoBinding) : ViewHolder(binding.root)
-
-    interface OnCoinClickListener {
-        fun onCoinClick(coin: CoinPriceInfo)
     }
 }
