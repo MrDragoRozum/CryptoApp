8afd81886197f329772161660cbe85d4b4c88226
Alexander Rozum <roozumaleks@gmail.com>
2023-09-22 18:02:20 +0300

    Добавлен фрагмент

--- app/src/main/java/com/example/cryptoapp/presentation/activities/CoinPriceListActivity.kt
+++ app/src/main/java/com/example/cryptoapp/presentation/activities/CoinPriceListActivity.kt
@@ -3,8 +3,11 @@ package com.example.cryptoapp.presentation.activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import com.example.cryptoapp.R
 import com.example.cryptoapp.databinding.ActivityCoinPriceListBinding
+import com.example.cryptoapp.domain.entities.CoinPriceInfo
 import com.example.cryptoapp.presentation.adapters.CoinInfoAdapter
+import com.example.cryptoapp.presentation.fragments.CoinDetailFragment
 import com.example.cryptoapp.presentation.viewmodels.ListCoinViewModel
 
 class CoinPriceListActivity : AppCompatActivity() {
@@ -29,8 +32,29 @@ class CoinPriceListActivity : AppCompatActivity() {
         viewModel.getTopCoinInfo.observe(this) { adapter.submitList(it) }
 
         adapter.onCoinClickListener = {
-            intent = CoinDetailActivity.newIntent(this, it.fromSymbol)
-            startActivity(intent)
+            if (isOnePaneMode()) {
+                launchActivityCoinDetail(it)
+            } else {
+                launchFragmentCoinDetail(it)
+            }
         }
     }
+
+    private fun launchActivityCoinDetail(it: CoinPriceInfo) {
+        val intent = CoinDetailActivity.newIntent(this, it.fromSymbol)
+        startActivity(intent)
+    }
+
+    private fun launchFragmentCoinDetail(it: CoinPriceInfo) {
+        supportFragmentManager.popBackStack()
+        supportFragmentManager
+            .beginTransaction()
+            .replace(
+                R.id.fragmentContainerViewCoinDetail,
+                CoinDetailFragment.newInstance(it.fromSymbol)
+            )
+            .addToBackStack(null).commit()
+    }
+
+    private fun isOnePaneMode() = binding.fragmentContainerViewCoinDetail == null
 }
